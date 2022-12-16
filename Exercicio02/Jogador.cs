500a565f98512048b4638fd399f057bab36aa89f
Pedro Henrique Custodio de Lima <lima.custodio@escolar.ifrn.edu.br>
2022-12-15 21:07:04 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio02/Jogador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio02
+{
+    class Jogador
+    {
+        private string nome;
+        private int gols;
+        public void SetNome(string n)
+        {
+            nome = n;
+        }
+        public void SetGols(int g)
+        {
+            if (g >= 0 ) gols = g;
+        }
+        public string GetNome()
+        {
+            return nome;
+        }
+        public int GetGols()
+        {
+            return gols;
+        }
+        public override string ToString()
+        {
+            if (gols == 0) return $"{nome} - nenhum gol";
+            if (gols == 1) return $"{nome} - um gol";
+            return $"{nome} - {gols} gols";
+        }
+    }
+}
