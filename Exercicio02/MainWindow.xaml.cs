2d9d0960246e5fd42ec45ac2df14929615393f0c
Pedro Henrique Custodio de Lima <lima.custodio@escolar.ifrn.edu.br>
2022-12-16 22:56:05 -0300

    projeto

--- Exercicio02/MainWindow.xaml.cs
+++ Exercicio02/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace Exercicio02
             InitializeComponent();
         }
         private Jogador[] js = new Jogador[10];
+        private time[] time = new time[10];
         private int k;
 
         private void Artilheiro_Click(object sender, RoutedEventArgs e)
@@ -49,10 +50,13 @@ namespace Exercicio02
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Jogador j = new Jogador();
+            time tm = new time();
             j.SetNome(txt1.Text);
             j.SetGols(int.Parse(txt2.Text));
             j.SetNumero(int.Parse(txt3.Text));
             j.SetAssistencia(int.Parse(txt4.Text));
+            tm.SetNome_time(txttime.Text);
+            tm.SetEstado(txtestado.Text);
             js[k] = j;
             k++;
             list.ItemsSource = null;
---
+++ Exercicio02/time.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio02
+{
+    class time
+    {
+        private string nome_time;
+        private string estado;
+        public void SetNome_time(string nt)
+        {
+            nome_time = nt;
+        }
+        public void SetEstado(string e)
+        {
+            estado = e;
+        }
+        public string GetNome_time()
+        {
+            return nome_time;
+        }
+        public string GetEstado()
+        {
+            return estado;
+        }
+        public override string ToString()
+        {
+            return $"Time: {nome_time} - Estado: {estado}";
+        }
+    }
+}
